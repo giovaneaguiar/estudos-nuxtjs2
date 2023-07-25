dec7a3c0e2fc32ac7fd1e1a4db6de61d98f1625c
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2023-07-25 11:41:57 -0300

    Páginas e rotas com Nuxt 2

---
+++ pages/a-empresa.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="bg-gray-100">
+            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+                <nuxt-link to="/a-empresa">A empresa</nuxt-link>
+                <nuxt-link to="/servicos">Serviços</nuxt-link>
+            </div>
+        </div>
+        
+
+        <div>
+            <div class="container mx-auto">
+                <h1 class="text-lg font-bold" >A empresa</h1>
+                <p>Hoje o AirBnB é considerado maior negócio de hospedagem do mundo, mesmo não sendo proprietário de nenhum quarto. 
+                    São mais de 150 milhões de usuários cadastrados, mais de 650 mil anfitriões oferecendo acomodações e 2 milhões de 
+                    pessoas se hospedando a cada dia.</p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/servicos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="bg-gray-100">
+            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+                <nuxt-link to="/a-empresa">A empresa</nuxt-link>
+                <nuxt-link to="/servicos">Serviços</nuxt-link>
+            </div>
+        </div>
+        
+
+        <div>
+            <div class="container mx-auto">
+                <h1 class="text-lg font-bold" >Serviços</h1>
+                <p>Serviços são as atividades econômicas que atendem a uma determinada demanda de mercado sem a 
+                    necessidade de produzir ou oferecer uma mercadoria. </p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
