9ae27c23665c68975df283d8d992ed165692e536
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2023-07-25 22:17:12 -0300

    Utilização de rotas-filho

---
+++ pages/servicos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="bg-gray-100">
+            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
+                <nuxt-link to="/a-empresa">A empresa</nuxt-link>
+                <nuxt-link to="/servicos">Serviços</nuxt-link>
+                <nuxt-link to="/produtos">Produtos</nuxt-link>
+            </div>
+        </div>
+        
+
+        <div>
+            <div class="container mx-auto">
+                <h1 class="text-lg font-bold" >Serviços</h1>
+
+                    <nuxt-link to="/servicos/desenvolvimento-de-sites">Desenvolvimento de Sites</nuxt-link>
+
+                    <nuxt-link to="/servicos/rh">Recursos Humanos</nuxt-link>
+                    
+                    <br><br>
+
+                    <nuxt-child />
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/servicos/index.vue
+++ pages/servicos/index.vue
@@ -1,29 +1,7 @@
 <template>
     <div>
-        <div class="bg-gray-100">
-            <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-                <nuxt-link to="/a-empresa">A empresa</nuxt-link>
-                <nuxt-link to="/servicos">Serviços</nuxt-link>
-                <nuxt-link to="/produtos">Produtos</nuxt-link>
-            </div>
-        </div>
-        
-
-        <div>
-            <div class="container mx-auto">
-                <h1 class="text-lg font-bold" >Serviços</h1>
-                
-                <p>Serviços são as atividades econômicas que atendem a uma determinada demanda de mercado sem a 
-                    necessidade de produzir ou oferecer uma mercadoria. </p>
-
-                    <nuxt-link to="/servicos/desenvolvimento-de-sites">Desenvolvimento de Sites</nuxt-link>
-                    Desenvolvimento de Sites
-
-                    <nuxt-link to="/servicos/rh">Recursos Humanos</nuxt-link>
-                    Recursos Humanos
-            </div>dutos
-        </div>
-
+        <p>Serviços são as atividades econômicas que atendem a uma determinada demanda de mercado sem a 
+         necessidade de produzir ou oferecer uma mercadoria. </p>
     </div>
 </template>
 
